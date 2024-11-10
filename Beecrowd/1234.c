fa7ff5f36789935bc94ad478959e41d2777729e1
Nicolas-David <nicolas.david@unifesp.br>
2024-11-10 20:00:46 -0300

    Create 1234.c

---
+++ Beecrowd/1234.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <ctype.h>  // Para as funções toupper e tolower
+
+void transformar_sentenca(char* sentenca) {
+    int i = 0;
+    int alternar = 1;  // Inicia com 1 para fazer a primeira letra ser maiúscula
+
+    while (sentenca[i] != '\0') {
+        if (sentenca[i] != ' ') {  
+            if (alternar) {
+                sentenca[i] = toupper(sentenca[i]);  // Torna maiúscula
+            } else {
+                sentenca[i] = tolower(sentenca[i]);  // Torna minúscula
+            }
+            alternar = !alternar;  // Vai alternando as letras
+        }
+        i++;
+    }
+}
+
+int main() {
+    char sentenca[51];  
+
+    while (fgets(sentenca, sizeof(sentenca), stdin)) {
+        size_t len = strlen(sentenca);
+        if (len > 0 && sentenca[len - 1] == '\n') {
+            sentenca[len - 1] = '\0';
+        }
+
+        // Função para transformar a sentença
+        transformar_sentenca(sentenca);
+
+        // Impressão
+        printf("%s\n", sentenca);
+    }
+
+    return 0;
+}

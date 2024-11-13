86a046c499221a5bb08bc49b811585b1b8cab700
Nicolas-David <nicolas.david@unifesp.br>
2024-11-13 17:41:41 -0300

    Update 1068.c

--- Beecrowd/1068.c
+++ Beecrowd/1068.c
@@ -1,3 +1,13 @@
+/*
+  * nome: 2460.c
+  * descrição: Balanço de Parenteses
+  * data: 13/11/2024
+  *Resolvido por Nicolas David da Cruz Santos
+  */
+
+
+
+
 #include <string.h>
 #include <stdio.h>
 
@@ -5,16 +15,16 @@ int main(){
     char expressao[1001];
     int i, tam, parenteses;
 
-    while(scanf("%s\n", &expressao) != EOF){
-        parenteses = 0;
-        tam = strlen(expressao);
+    while(scanf("%s\n", &expressao) != EOF){//Lê até que não aja mais o que ler
+        parenteses = 0;// Começa em 0 o número de parenteses que vão sobrar
+        tam = strlen(expressao); // tamanho da expressão
 
         for(i = 0; i < tam; ++i){
             if(expressao[i] == '('){
-                ++parenteses;
-            }else if(expressao[i] == ')'){
-                if(parenteses > 0)  --parenteses;
-                else                break;
+                ++parenteses;// Se for ( adciona ao parenteses
+            }else if(expressao[i] == ')'){//se ) retira dos parenteses
+                if(parenteses > 0)  --parenteses;// Garante tbm que o número seja positivo
+                else                break;// Quebra o looping se for negativo!
             }
         }
 

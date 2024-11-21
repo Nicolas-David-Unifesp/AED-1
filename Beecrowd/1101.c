8b6fbb0c9d0dcfc0f14220f2cb90c30d6e2d5dca
Nicolas-David <nicolas.david@unifesp.br>
2024-11-21 08:34:19 -0300

    Create 1101.c

---
+++ Beecrowd/1101.c
@@ -0,0 +1,31 @@
+
+
+
+#include <stdio.h>
+
+int main() {
+    int M, N;
+
+    while (1) { // Loop infinito, será encerrado quando M E N menor/igual a zero
+        scanf("%d %d", &M, &N);
+
+        // Condição de parada
+        if (M <= 0 || N <= 0) {
+            break;
+        }
+
+        // Vendo quem é o menor e o maior
+        int menor = (M < N) ? M : N;
+        int maior = (M > N) ? M : N;
+
+        // Cálculo e impressão da sequência de números e a soma entre eles
+        int soma = 0;
+        for (int i = menor; i <= maior; i++) {
+            printf("%d ", i);
+            soma += i;
+        }
+        printf("Sum=%d\n", soma);
+    }
+
+    return 0;
+}

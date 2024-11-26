539c613aa1c527a9eb4cfe6e2893b26ce216b65c
Nicolas-David <nicolas.david@unifesp.br>
2024-11-26 20:20:35 -0300

    Atualizar o time3.c

--- .vscode/time3.c
+++ .vscode/time3.c
@@ -1,1 +1,14 @@
-.
+void Intercala (int p, int q, int r, int v[]) {
+int i, j, k, *w;
+w = malloc ((r-p) * sizeof (int));
+i = p; j = q; k = 0;
+while (i < q && j < r) {
+if (v[i] <= v[j]) w[k++] = v[i++];
+else w[k++] = v[j++];
+}
+while (i < q) w[k++] = v[i++];
+while (j < r) w[k++] = v[j++];
+for (i = p; i < r; i++) v[i] = w[i-p];
+free (w);
+}
+

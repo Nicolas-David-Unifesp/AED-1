abe8ea38f801bd38f3efb65f203467d162884c0b
Nicolas-David <nicolas.david@unifesp.br>
2024-11-26 19:18:49 -0300

    Update time2.c

--- .vscode/time2.c
+++ .vscode/time2.c
@@ -1,1 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h> //clock(), CLOCKS_PER_SEC e clock_t
 
+int const TAM = 100000; //constante para tamanho do vetor
+
+void Inserção (int n, int v[]) {
+  int i, j, x;
+  for (j = 1; /*A*/ j < n; j++) {
+    x = v[j];
+      for (i = j-1; i >= 0 && v[i] > x; i--){
+        v[i+1] = v[i];
+        v[i+1] = x;
+      }
+   }
+}
+
+
+
+int main(){
+    clock_t t, t1; //variável para armazenar tempo
+    int vetor[TAM], a, j, k;
+    
+    //semente de aleatoriedade
+    srand((unsigned)time(NULL));
+    //geração aleatório dos valores do vetor
+    for(int a = 0; a < TAM; a++){
+        vetor[a] = rand() % TAM;
+    }
+    //Verificando tempo de execução do bubble sort=> t2
+    t = clock(); //armazena tempo
+    Inserção(TAM, vetor);
+    t = clock() - t; //tempo final - tempo 
+
+    //imprime o tempo na tela
+    printf("Tempo de execucao: %lf ms\n", ((double)t)/((CLOCKS_PER_SEC/1000))); //conversão para double
+
+    return 0;
+}

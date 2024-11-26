dea330ce978b4957b4ffe67f9e8220553619cc8f
Nicolas-David <nicolas.david@unifesp.br>
2024-11-26 20:22:24 -0300

    Update time3.c

--- .vscode/time3.c
+++ .vscode/time3.c
@@ -1,23 +1,23 @@
 void Intercala (int p, int q, int r, int v[]) {
-int i, j, k, *w;
-w = malloc ((r-p) * sizeof (int));
-i = p; j = q; k = 0;
-while (i < q && j < r) {
-if (v[i] <= v[j]) w[k++] = v[i++];
-else w[k++] = v[j++];
-}
-while (i < q) w[k++] = v[i++];
-while (j < r) w[k++] = v[j++];
-for (i = p; i < r; i++) v[i] = w[i-p];
-free (w);
+  int i, j, k, *w;
+  w = malloc ((r-p) * sizeof (int));
+  i = p; j = q; k = 0;
+  while (i < q && j < r) {
+    if (v[i] <= v[j]) w[k++] = v[i++];
+    else w[k++] = v[j++];
+  }
+  while (i < q) w[k++] = v[i++];
+  while (j < r) w[k++] = v[j++];
+  for (i = p; i < r; i++) v[i] = w[i-p];
+  free (w);
 }
 
 void Mergesort (int p, int r, int v[]) {
-if (p < r - 1) {
-int q = (p + r)/2;
-Mergesort (p, q, v);
-Mergesort (q, r, v);
-Intercala (p, q, r, v);
-}
+  if (p < r - 1) {
+    int q = (p + r)/2;
+    Mergesort (p, q, v);
+    Mergesort (q, r, v);
+    Intercala (p, q, r, v);
+  }
 }
 

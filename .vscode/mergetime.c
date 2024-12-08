f96e15ad29c8c42e75db0be4c35a5473e5ef77ef
Nicolas-David <nicolas.david@unifesp.br>
2024-12-08 17:12:06 -0300

    Update mergetime.c

--- .vscode/mergetime.c
+++ .vscode/mergetime.c
@@ -5,9 +5,9 @@
 int const TAM = 100000; // Tamanho
 
 // Função Mescla Dois Arrajos
-void Intercala (int p, int q, int r, int v[]) {
+void Intercala (int v[], int p, int q, int r) {
   int i, j, k, *w;
-  w = malloc ((r-p) * sizeof (int));
+  w = malloc ((r-p+1) * sizeof (int));
   i = p; j = q; k = 0;
   while (i < q && j < r) {
     if (v[i] <= v[j]) w[k++] = v[i++];
@@ -18,32 +18,33 @@ void Intercala (int p, int q, int r, int v[]) {
   for (i = p; i < r; i++) v[i] = w[i-p];
   free (w);
 }
+
 // Função MergeSort
 void mergesort(int v[], int baixo, int alto) {
     if (baixo < alto) {
         int meio = baixo + (alto - baixo) / 2;
-        mergesort(v, baixo, meio);
-        mergesort(v, meio + 1, alto);
-        Intercala(v, baixo, meio, alto);
+        mergesort(v, baixo, meio);               
+        mergesort(v, meio + 1, alto);             
+        Intercala(v, baixo, meio + 1, alto + 1);  
     }
 }
 
 int main() {
-    clock_t t; // variável para armazenar tempo
+    clock_t t;
     int vetor[TAM];
 
-    // semente de aleatoriedade
+    // Valores Aleatórios
     srand((unsigned)time(NULL));
 
-    // geração aleatória dos valores do vetor
+    // Vai Atribuindo
     for (int a = 0; a < TAM; a++) {
         vetor[a] = rand() % TAM;
     }
 
-    // Verificando tempo de execução do MergeSort
-    t = clock(); // armazena tempo inicial
+    // Tempo
+    t = clock(); 
     mergesort(vetor, 0, TAM - 1);
-    t = clock() - t; // tempo final - tempo inicial
+    t = clock() - t; // Delta Tempo
 
     // imprime o tempo na tela
     printf("Tempo de execucao: %lf ms\n", ((double)t) / (CLOCKS_PER_SEC / 1000)); // conversão para double

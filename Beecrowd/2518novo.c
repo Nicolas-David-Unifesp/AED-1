326b919ad307c145f6d7fa1706fd665ef3b0d4b9
Nicolas-David <nicolas.david@unifesp.br>
2025-01-15 15:48:12 -0300

    Update 2518novo.c

--- Beecrowd/2518novo.c
+++ Beecrowd/2518novo.c
@@ -1,13 +1,16 @@
-//2518
+/*
+  * nome: 2518.c
+  * descrição:
+  * data: 15/01/2025
+  *Resolvido por Nicolas David da Cruz Santos
+*/
 #include <stdio.h>
 #include <math.h>
-int main()
-{
+int main(){
 	int n;
 	int h, c, l;
 	double hip, area;
-	while(scanf("%d",&n)!=EOF)
-	{
+	while(scanf("%d",&n)!=EOF){
 		scanf("%d%d%d",&h,&c,&l);
 		hip = sqrt(pow(h,2)+pow(c,2));
 		area = (l*hip*n)/10000.0;

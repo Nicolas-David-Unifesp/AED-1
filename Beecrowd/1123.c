e4615679a538c4dd7d213b966c8d73bfe6d2a03e
Nicolas-David <nicolas.david@unifesp.br>
2025-01-21 11:07:46 -0300

    Desvio de Rota

--- Beecrowd/1123.c
+++ Beecrowd/1123.c
@@ -5,84 +5,120 @@
   *Resolvido por Nicolas David da Cruz Santos
   */
 
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <limits.h>
 
-#define MAXN 250
+#define MAXN 260
+#define LIMITE 9999999
 
+//**************************************************************************//
 
 typedef struct {
-    int cidade1, cidade2, pedagio;
-} Estrada;
+    int cidade, custo;
+} ii;
 
-// dijkstra- encontra o menor custo
-int dijkstra(int numCidades, int numServicos, int cidadeConsertada, Estrada estradas[], int numEstradas) {
-    int distancias[MAXN];
-    int visitado[MAXN] = {0}; 
-    int adjacencia[MAXN][MAXN]; // Matriz pra representar as estradas
+typedef struct {
+    ii elementos[MAXN * MAXN];
+    int tamanho;
+} MinHeap;// HeapMin
 
-    // Coloca as distâncias ao infinito
-    for (int i = 0; i < numCidades; i++) {
-        distancias[i] = INT_MAX;
-        for (int j = 0; j < numCidades; j++) {
-            adjacencia[i][j] = INT_MAX;
+//**************************************************************************//
+
+void inserir(MinHeap *heap, ii elemento) {// Inserção Heap Min
+    heap->elementos[heap->tamanho] = elemento;
+    int i = heap->tamanho;
+    heap->tamanho++;
+
+    while (i != 0 && heap->elementos[i].custo < heap->elementos[(i - 1) / 2].custo) {
+        ii temp = heap->elementos[i];
+        heap->elementos[i] = heap->elementos[(i - 1) / 2];
+        heap->elementos[(i - 1) / 2] = temp;
+        i = (i - 1) / 2;
+    }
+}
+
+//**************************************************************************//
+
+ii extrairMinimo(MinHeap *heap) {// Extrai o elemento mínimo do heap min
+    ii raiz = heap->elementos[0];
+    heap->elementos[0] = heap->elementos[heap->tamanho - 1];
+    heap->tamanho--;
+    int i = 0;
+
+    while (2 * i + 1 < heap->tamanho) {
+        int menor = 2 * i + 1;
+        if (menor + 1 < heap->tamanho && heap->elementos[menor + 1].custo < heap->elementos[menor].custo) {
+            menor++;
         }
+        if (heap->elementos[i].custo <= heap->elementos[menor].custo) {
+            break;
+        }
+        ii temp = heap->elementos[i];
+        heap->elementos[i] = heap->elementos[menor];
+        heap->elementos[menor] = temp;
+        i = menor;
     }
 
-    // Define os valores conforme o que é fornecido
-    for (int i = 0; i < numEstradas; i++) {
-        adjacencia[estradas[i].cidade1][estradas[i].cidade2] = estradas[i].pedagio;
-        adjacencia[estradas[i].cidade2][estradas[i].cidade1] = estradas[i].pedagio;
-    }
+    return raiz;
+}
+//**************************************************************************//
 
-    distancias[cidadeConsertada] = 0; // distância da cidade concertada é zero, né
+// Variaveis Globais ( Tive que colocá-las aqui para ficar mais organizado)
+int visitado[MAXN];
+ii grafo[MAXN][MAXN];
+int grau[MAXN];
 
-    // Algoritmo de Dijkstra
-    for (int i = 0; i < numCidades; i++) {
-        int cidadeAtual = -1;
-        
-        // Encontra a cidade não visitada com a menor distância
-        for (int j = 0; j < numCidades; j++) {
-            if (!visitado[j] && (cidadeAtual == -1 || distancias[j] < distancias[cidadeAtual])) {
-                cidadeAtual = j;
+// ************************************************************************//
+
+int main() {
+    int n, m, c, k;
+    while (scanf("%d %d %d %d", &n, &m, &c, &k) && (n || m || c || k)) {// Vai lendo até 0 0 0 0
+        for (int i = 0; i <= n; i++) {
+            grau[i] = 0;
+            visitado[i] = 0;
+        }
+
+        while (m--) {// Construindo o Grafo
+            int u, v, peso;
+            scanf("%d %d %d", &u, &v, &peso);
+            grafo[u][grau[u]].cidade = v;
+            grafo[u][grau[u]++].custo = peso;
+            grafo[v][grau[v]].cidade = u;
+            grafo[v][grau[v]++].custo = peso;
+        }
+
+        MinHeap dijkstra;
+        dijkstra.tamanho = 0;
+        ii inicial = {k, 0};
+        inserir(&dijkstra, inicial);
+     
+        // DIJSKTRA
+        while (dijkstra.tamanho > 0) {
+            ii davez = extrairMinimo(&dijkstra);
+            if (visitado[davez.cidade]) continue;// Se já foi visitado pula
+            visitado[davez.cidade] = 1;// Marca como Visitado
+            if (davez.cidade == c - 1) {// Aqui é para caso chegue ao destino, aí quebra o ciclo
+                printf("%d\n", davez.custo);
+                break;
             }
-        }
-
-        // Se a distância for infinita interrompe o loop
-        if (distancias[cidadeAtual] == INT_MAX) break;
-
-        visitado[cidadeAtual] = 1; // Vai maracndo a cidade como visitada
-
-        // Atualiza as distâncias
-        for (int v = 0; v < numCidades; v++) {
-            if (!visitado[v] && adjacencia[cidadeAtual][v] != INT_MAX && distancias[cidadeAtual] + adjacencia[cidadeAtual][v] < distancias[v]) {
-                // Vamos verificar se a cidade faz parte da rota de serviço!!
-                if (cidadeAtual >= 0 && cidadeAtual < numServicos && v == cidadeAtual + 1) {
-                    distancias[v] = distancias[cidadeAtual] + adjacencia[cidadeAtual][v];
-                } else if (cidadeAtual >= numServicos || v >= numServicos) {
-                    distancias[v] = distancias[cidadeAtual] + adjacencia[cidadeAtual][v];
+            // Se está na rota de serviço, só vamos considerar a rota!!!
+            if (davez.cidade <= c - 1) {
+                for (int i = 0; i < grau[davez.cidade]; i++) {
+                    if (grafo[davez.cidade][i].cidade == davez.cidade + 1) {
+                        ii novo = {davez.cidade + 1, grafo[davez.cidade][i].custo + davez.custo};
+                        inserir(&dijkstra, novo);
+                        break;
+                    }
+                }
+            } else {// Caso não esteja, considera o resto!
+                for (int i = 0; i < grau[davez.cidade]; i++) {
+                    ii novo = {grafo[davez.cidade][i].cidade, grafo[davez.cidade][i].custo + davez.custo};
+                    inserir(&dijkstra, novo);
                 }
             }
         }
-    }
-
-    // Retorna o custo mínimo
-    return distancias[numServicos - 1];
-}
-
-int main() {
-    int numCidades, numEstradas, numServicos, cidadeConsertada;
-    while (scanf("%d %d %d %d", &numCidades, &numEstradas, &numServicos, &cidadeConsertada) && numCidades && numEstradas && numServicos && cidadeConsertada) {
-        Estrada estradas[numEstradas];
-
-        for (int i = 0; i < numEstradas; i++) {
-            scanf("%d %d %d", &estradas[i].cidade1, &estradas[i].cidade2, &estradas[i].pedagio);
-        }
-
-        // Calcula e imprime o valor do pedágio
-        int resultado = dijkstra(numCidades, numServicos, cidadeConsertada, estradas, numEstradas);
-        printf("%d\n", resultado);
     }
 
     return 0;

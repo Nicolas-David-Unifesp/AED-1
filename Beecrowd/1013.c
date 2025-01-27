3e0038a747f7eec92f3bd0a428cd56c45075e56a
Nicolas-David <nicolas.david@unifesp.br>
2025-01-27 09:17:50 -0300

    Create 1013.c

---
+++ Beecrowd/1013.c
@@ -0,0 +1,17 @@
+/*
+  * nome: 1013.c
+  * descrição: O Maior
+  * data: 27/01/2025
+  *Resolvido por Nicolas David da Cruz Santos
+  */
+#include <stdio.h>
+#include <stdlib.h> 
+
+int main() {
+    int a, b, c, maiorAB, maior;
+    scanf("%d %d %d", &a, &b, &c);
+    maiorAB = (a + b + abs(a - b)) / 2;
+    maior = (maiorAB + c + abs(maiorAB - c)) / 2;
+    printf("%d eh o maior\n", maior);
+    return 0;
+}

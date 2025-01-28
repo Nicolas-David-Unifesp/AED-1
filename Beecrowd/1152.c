59859bc3cffecdcbcb60ebc8f012f49d44efc31f
Nicolas-David <nicolas.david@unifesp.br>
2025-01-28 20:11:07 -0300

    Update 1152.c

--- Beecrowd/1152.c
+++ Beecrowd/1152.c
@@ -39,37 +39,39 @@ int compara(const void *a, const void *b) { // substituir por uma função de or
     return aresta1->w - aresta2->w; // a função ordena de modo crescente a partir da diferença dos pesos das arestas
 }
 
-int main() {
-    int V, A;
-
-    while (scanf("%d %d", &V, &A), V && A) // o && indica que a função é verdadeira enquanto V e A forem verdadeiros, ou seja, != 0
-    {
-        int tot = 0;
-
-        // Aloca memória dinamicamente com base no número de arestas e vértices
-        g = (Aresta *)malloc(A * sizeof(Aresta)); // armazena as arestas// Vetor Dinâmico
-        p = (int *)malloc(V * sizeof(int));       // armazena os pais (Union-Find)// Vértices
-
-        for (int i = 0; i < A; i++) { // percorre todas as arestas e as adiciona na struct arestas dentro de g
+void AddStruct(int A, int tot){
+    for (int i = 0; i < A; i++) { // percorre todas as arestas e as adiciona na struct arestas dentro de g
             scanf("%d %d %d", &g[i].u, &g[i].v, &g[i].w);
             tot += g[i].w; // tamanho total das arestas
-        }
-
-        qsort(g, A, sizeof(Aresta), compara); // g-primeiro elemento do array, A-número de elementos, tamanho dos elementos, função compara
-
-        for (int i = 0; i < V; i++) { // define os parentes iniciais
+    }
+}
+void DefinePaiInicial(int V){
+    for (int i = 0; i < V; i++) { // define os parentes iniciais
             p[i] = i;
-        }
+    }
+}
 
+int main() {
+    int V, A;// V = Verices e A = Arestas
+
+    while (scanf("%d %d", &V, &A), V && A) // repete enquanto V e A != 0
+    {
+        int tot = 0;
+        // Alocação Dinâmica com base no número de arestas e vértices
+        g = (Aresta *)malloc(A * sizeof(Aresta)); // armazena as arestas// Vetor Dinâmico
+        p = (int *)malloc(V * sizeof(int));       // armazena os pais (Union-Find)// Vértices
+        
+        AddStruct(A, tot);//Add na struct arestas em g
+        
+        qsort(g, A, sizeof(Aresta), compara); // g-primeiro elemento do array, A-número de elementos, tamanho dos elementos, função compara
+        
+        DefinePaiInicial(V);// Para o Union-Find
+        
         printf("%d\n", tot - kruskal(A)); // número da economia para Kruskal pelo número de arestas
-
-        // Libera a memória alocada
+        
         free(g);
         free(p);
     }
 
     return 0;
 }
-
-
-//utilizei union fins que ainbda não entendo por completo mas foli o unico jeito que encontrei para utilizar kruskal
